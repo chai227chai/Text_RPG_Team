20ca8249e3832250775ddf495ff37ced0ac6e5d9
jihye <thoth_0302@naver.com>
2024-01-09 18:27:43 +0900

    class 포션 생성
    포션 생성 및 포션 사용 함수 생성

---
+++ Text_RPG_Team/Portion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Text_RPG_Team
+{
+    internal class Portion
+    {
+        public int myportion;
+
+        public Portion(int n)
+        {
+            myportion = n;
+        }
+
+        //포션 생성
+        private void GetPortion(int n)
+        {
+            myportion += n;
+        }
+
+        //포션 사용
+        public void UsePortion(Player player)
+        {
+            if(myportion <= 0) 
+            {
+                Console.WriteLine("포션이 부족합니다.");
+            }
+            else
+            {
+                if (player.Health > 70)
+                {
+                    player.Health = 100;
+                }
+                else player.Health += 30;
+
+                myportion--;
+                Console.WriteLine("회복을 완료했습니다.");
+            }
+        }
+    }
+}
