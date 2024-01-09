b36235512cb6bc11c86d70306cb0bd7a9a9593e9
jihye <thoth_0302@naver.com>
2024-01-09 20:17:29 +0900

    최대 체력일 경우 회복 포션 사용 안하도록 설정

--- Text_RPG_Team/Portion.cs
+++ Text_RPG_Team/Portion.cs
@@ -18,7 +18,7 @@ namespace Text_RPG_Team
             myportion = n;
         }
 
-        //포션 생성
+        //포션 생성 , 던전 드랍용
         private void GetPortion(int n)
         {
             myportion += n;
@@ -27,13 +27,21 @@ namespace Text_RPG_Team
         //포션 사용
         public void UsePortion(Player player)
         {
+            int beforehealth = player.Health;
             if(myportion <= 0) 
             {
                 Console.WriteLine("포션이 부족합니다.");
             }
             else
             {
-                if (player.MaxHealth - player.Health < 30)
+                if(player.MaxHealth == player.Health)
+                {
+                    Console.Clear();
+                    Console.WriteLine("이미 최대 체력입니다.");
+                    Thread.Sleep(1000);
+                    return;
+                }
+                else if (player.MaxHealth - player.Health < 30)
                 {
                     player.Health = player.MaxHealth;
                 }
@@ -41,6 +49,8 @@ namespace Text_RPG_Team
 
                 myportion--;
                 Console.WriteLine("회복을 완료했습니다.");
+                Console.WriteLine($"{beforehealth} -> {player.Health}");
+                Thread.Sleep(1000);
             }
         }
     }
