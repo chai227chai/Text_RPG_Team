0daef4f11b1236c112d972331d6286bfe13edceb
jihye <thoth_0302@naver.com>
2024-01-09 16:21:18 +0900

    초기화면 추가

--- Text_RPG_Team/GameManager.cs
+++ Text_RPG_Team/GameManager.cs
@@ -8,14 +8,43 @@ namespace Text_RPG_Team
 {
     internal class GameManager
     {
+        Player character = new Player();
+
         public GameManager()
         {
+            FirstScreen();
+            character.Name = SetCharacter();
             MainTown();
         }
 
-
         //---------------------------------------------------------------------------------------------------------------
         //게임 시작 초기 화면 함수
+        void FirstScreen()
+        {
+            Console.WriteLine("=============================================================================================");
+            Console.WriteLine(" /$$$$$$$$/$$$$$$$$ /$$   /$$ /$$$$$$$$        /$$$$$$   /$$$$$$  /$$      /$$ /$$$$$$$$");
+            Console.WriteLine("|__  $$__/ $$_____/| $$  / $$|__  $$__/       /$$__  $$ /$$__  $$| $$$    /$$$| $$_____/");
+            Console.WriteLine("   | $$  | $$      |  $$/ $$/   | $$         | $$  \\__/| $$  \\ $$| $$$$  /$$$$| $$      ");
+            Console.WriteLine("   | $$  | $$$$$    \\  $$$$/    | $$         | $$ /$$$$| $$$$$$$$| $$ $$/$$ $$| $$$$$   ");
+            Console.WriteLine("   | $$  | $$__/     >$$  $$    | $$         | $$|_  $$| $$__  $$| $$  $$$| $$| $$__/   ");
+            Console.WriteLine("   | $$  | $$       /$$/\\  $$   | $$         | $$  \\ $$| $$  | $$| $$\\  $ | $$| $$      ");
+            Console.WriteLine("   | $$  | $$$$$$$$| $$  \\ $$   | $$         |  $$$$$$/| $$  | $$| $$ \\/  | $$| $$$$$$$$");
+            Console.WriteLine("   |__/  |________/|__/  |__/   |__/          \\______/ |__/  |__/|__/     |__/|________/");
+            Console.WriteLine("=============================================================================================");
+            Console.WriteLine(PadLeftForMixedText("Press Any Key to Play", 93));
+            Console.WriteLine("=============================================================================================");
+            Console.ReadKey();
+        }
+
+        public string SetCharacter()
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("원하시는 이름을 설정해주세요.");
+            Console.Write(">>");
+            return Console.ReadLine();
+        }
+
         private void MainTown()
         {
             Console.Clear();
@@ -34,16 +63,16 @@ namespace Text_RPG_Team
             switch (act)
             {
                 case 1:
+                    character.ViewState();
                     break;
                 case 2:
                     break;
             }
         }
 
-
-        //---------------------------------------------------------------------------------------------------------------
-        //입력이 올바른지 확인하는 함수
-        public int IsValidInput(int max, int min)
+            //---------------------------------------------------------------------------------------------------------------
+            //입력이 올바른지 확인하는 함수
+            public int IsValidInput(int max, int min)
         {
             int keyInput;
             bool result;
@@ -76,6 +105,39 @@ namespace Text_RPG_Team
             }
 
             return false;
+        }
+
+        //---------------------------------------------------------------------------------------------------------------
+        //출력 정렬하는 함수
+        private int GetPrintableLength(string str)
+        {
+            int length = 0;
+            foreach (char c in str)
+            {
+                if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                {
+                    length += 2;
+                }
+                else
+                {
+                    length += 1;
+                }
+            }
+            return length;
+        }
+
+        private string PadLeftForMixedText(string str, int totalLength)
+        {
+            int currentLength = GetPrintableLength(str);
+            int padding = (totalLength - currentLength) / 2;
+            return str.PadLeft(str.Length + padding);
+        }
+
+        private string PadRightForMixedText(string str, int totalLength)
+        {
+            int currentLength = GetPrintableLength(str);
+            int padding = (totalLength - currentLength) / 2;
+            return str.PadRight(str.Length + padding);
         }
     }
 }
