052e6c5b6f2fca2c4a180d53a0bec0ca5113ac30
chai227chai <chai227chai@naver.com>
2024-01-11 19:08:43 +0900

    아이템 능력치 추가 개선

--- Text_RPG_Team/Item.cs
+++ Text_RPG_Team/Item.cs
@@ -118,21 +118,26 @@ namespace Text_RPG_Team
         }
 
         //아이템 능력치 종류
-        public SpecType GetSpecType
+        //public SpecType GetSpecType
+        //{
+        //    get { return spec.getSpecType(); }
+        //}
+
+        public ItemSpec Spec
         {
-            get { return spec.getSpecType(); }
+            get { return spec; }
         }
 
         //아이템 능력치 수치
-        public int GetSpec
+        public int GetSpec(SpecType specType)
         {
-            get { return spec.getSpec(); }
+            return spec.GetSpec(specType);
         }
 
         //아이템 능력치 종류 출력
-        public string GetSpecName
+        public void GetSpecName()
         {
-            get { return spec.getSpecName(); }
+            spec.getSpecList();
         }
 
         //아이템 장착 여부
