f16c927b017994ce7e13edb2a319f80db47a8958
Rivene <abcfgf777@naver.com>
2024-01-12 13:04:37 +0900

    게임 매니저 콘솔 꾸미기

--- Text_RPG_Team/GameManager.cs
+++ Text_RPG_Team/GameManager.cs
@@ -59,7 +59,7 @@ namespace Text_RPG_Team
             Console.Clear();
             Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
             Console.WriteLine("원하시는 이름을 설정해주세요.");
-            Console.Write(">>");
+            Console.Write(">> ");
             return Console.ReadLine();
         }
         public void SetJob() //직업선택후 직업에 따른스탯변경
@@ -67,7 +67,13 @@ namespace Text_RPG_Team
             Console.Clear();
             Console.WriteLine("캐릭터의 직업을 선택해 주세요");
             Console.WriteLine();
-            Console.WriteLine("1.전사 2.마법사 3.도적");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("1.전사 ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("2.마법사 ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("3.도적");
+            Console.ResetColor();
 
             Console.WriteLine();
           
@@ -115,11 +121,13 @@ namespace Text_RPG_Team
 
             Console.WriteLine();
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("1. 상태보기");
             Console.WriteLine($"2. 전투시작  (현재 층 수 : {dungeon.Now_Stage}층)");
             Console.WriteLine("3. 회복 아이템");
             Console.WriteLine("4. 인벤토리");
             Console.WriteLine("5. 상점");
+            Console.ResetColor();
 
             Console.WriteLine();
 
@@ -167,7 +175,7 @@ namespace Text_RPG_Team
                     Console.WriteLine("잘못된 입력입니다.");
                     Console.WriteLine("다시 입력해 주세요.");
                 }
-                Console.Write(">>");
+                Console.Write(">> ");
                 result = int.TryParse(Console.ReadLine(), out keyInput);
 
                 cnt = 1;
@@ -192,8 +200,11 @@ namespace Text_RPG_Team
         private void ViewPlayer()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("■상태보기■");
+            Console.ResetColor();
             Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
             Console.WriteLine("LV : " + character.Level.ToString("00"));
             Console.WriteLine($"{character.Name} ( {character.GetJob} )");
@@ -206,6 +217,7 @@ namespace Text_RPG_Team
             Console.Write($"속  도 : {character.Total_Speed}");
             Console.WriteLine((character.PlusSpeed != 0) ? $" ( {(character.PlusSpeed > 0 ? "+" : "")} {character.PlusSpeed})" : "");
             Console.WriteLine($"Gold : {character.Gold}G");
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
@@ -223,8 +235,11 @@ namespace Text_RPG_Team
         private void EnterDungeon()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("■던전입장■");
+            Console.ResetColor();
             Console.WriteLine("던전에 도전하여 보상을 획득할 수 있습니다.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
 
             Console.WriteLine($"현재 {dungeon.Now_Stage}층에 도전 중 입니다.");
@@ -238,6 +253,7 @@ namespace Text_RPG_Team
             Console.WriteLine($"체  력 : {character.Health}");
             Console.WriteLine($"마  나 : {character.Mp}");
             Console.WriteLine($"속  도 : {character.Total_Speed}");
+            Console.ResetColor();
             Console.WriteLine();
 
             Console.WriteLine();
@@ -266,9 +282,14 @@ namespace Text_RPG_Team
         void ResetDungeon()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("■던전초기화■");
+            Console.ResetColor();
             Console.WriteLine("던전을 초기화 합니다.");
-            Console.WriteLine("초기화 할 경우, 던전을 1층부터 다시 도전해야 합니다. 정말로 초기화 하시겠습니까?");
+            Console.WriteLine("초기화 할 경우, 던전을 1층부터 다시 도전해야 합니다. ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("정말 초기화 하시겠습니까?");
+            Console.ResetColor();
             Console.WriteLine();
 
             Console.WriteLine();
@@ -292,12 +313,16 @@ namespace Text_RPG_Team
         private void ViewPortion()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("■회복■");
+            Console.ResetColor();
             Console.WriteLine($"포션을 사용하면 체력을 30 회복할 수 있습니다");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"회복 가능한 최대 체력 : {character.MaxHealth}");
             Console.WriteLine($"현재 체력 : {character.Health}");
             Console.WriteLine();
+            Console.ResetColor();
             Console.WriteLine("1. 체력 회복하기");
             Console.WriteLine("2. 마나 회복하기");
             Console.WriteLine("0. 나가기");
@@ -321,15 +346,19 @@ namespace Text_RPG_Team
         private void ViewInventory()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("■인벤토리■");
+            Console.ResetColor();
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("[아이템 목록]");
             itemlist.PrintItemList(inventory.GetInventoryList);
             Console.WriteLine();
             Console.WriteLine($"체력 포션 - {hpportion.Count}개");
             Console.WriteLine($"마나 포션 - {mpportion.Count}개");
             Console.WriteLine();
+            Console.ResetColor();
             Console.WriteLine("1. 장착관리");
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
@@ -348,12 +377,16 @@ namespace Text_RPG_Team
         private void InventoryManager()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("■인벤토리 - 장착 관리■");
+            Console.ResetColor();
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("[아이템 목록]");
             itemlist.PrintItemList(inventory.GetInventoryList, true);
             Console.WriteLine("");
+            Console.ResetColor();
             Console.WriteLine("0. 나가기");
             Console.WriteLine("");
             int act = IsValidInput(inventory.GetInventoryList.Count, 0);
@@ -376,15 +409,19 @@ namespace Text_RPG_Team
         private void ViewStore()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("■상점■");
+            Console.ResetColor();
             Console.WriteLine("필요한 아이템을 얻거나 가지고 있는 아이템을 팔 수 있는 상점입니다.");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("[보유 골드]");
             Console.WriteLine($"{character.Gold}G");
             Console.WriteLine("");
             Console.WriteLine("[아이템 목록]");
             itemlist.PrintItemList(itemlist.GetItemList, false ,true);
             Console.WriteLine();
+            Console.ResetColor();
             Console.WriteLine("1. 아이템 구매");
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
@@ -404,14 +441,18 @@ namespace Text_RPG_Team
         private void StoreBuy()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("■상점 - 아이템 구매■");
+            Console.ResetColor();
             Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
             Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("[보유 골드]");
             Console.WriteLine($"{character.Gold}G");
             Console.WriteLine("");
             Console.WriteLine("[아이템 목록]");
             itemlist.PrintItemList(itemlist.GetItemList, true, true);
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
